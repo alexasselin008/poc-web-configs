2369d126018afa38864f2ce3b5ae0dd61ba44b76
Alexandre Asselin <alexandre.asselin@gsoft.com>
2023-01-06 12:39:13 -0500

    manuall y add rules and config

--- packages/eslint-plugin/index.ts
+++ packages/eslint-plugin/index.ts
@@ -1,9 +1,19 @@
-import requireIndex from "requireindex";
 import {ESLint} from "eslint";
 
 const plugin: ESLint.Plugin = {
-    rules: requireIndex("./lib/rules"),
-    configs: requireIndex("./lib/config")
+    rules: { 
+        "prefer-cx": require("./lib/rules/prefer-cx"),
+        "restrict-full-import": require("./lib/rules/restrict-full-import"),
+        "strict-css-modules-names": require("./lib/rules/strict-css-modules-names"),
+    },
+    configs: {
+        lodash: require("./lib/config/lodash"),
+        nextjs: require("./lib/config/nextjs"),
+        react: require("./lib/config/react"),
+        recommended: require("./lib/config/recommended"),
+        "sort-imports": require("./lib/config/sort-imports"),
+        typescript: require("./lib/config/typescript"),
+    }
 }
 
 export = plugin;
